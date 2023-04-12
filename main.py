3b272656b382397f1975eb545a5921e19265ec0a
Aviral-PC <aviral17@hotmail.com>
2023-04-12 13:14:42 -0700

    WebPage Title added

--- main.py
+++ main.py
@@ -13,17 +13,28 @@ response=requests.get(url_final)
 
 #get json content from the response of URL
 content=response.json()
-print(content)
+#print(content)
 
 #Get Image URL from json content
 image_url=content["url"]
-print(image_url)
+#print(image_url)
 
 #Get Image Description from json content
 image_description=content["explanation"]
 print(image_description)
 
+#Get Binary Data from Image url of the image
+img_binary_data=requests.get(image_url).content
+#print(img_binary_data)
+
+#Create image file
+with open("image.jpg","wb") as file:
+    file.write(img_binary_data)
+
 #Send Content to webpage
 
 st.set_page_config(page_title="Image of the Day",layout="wide")
+st.title("Image of the Day")
+st.image("image.jpg")
+st.write(image_description)
 
